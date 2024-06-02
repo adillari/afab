42140fa5e7f0975605977aa1935d31d971e73310
Adil Lari <adillari@pm.me>
2024-06-02 16:18:14 -0500

    define how link_cleaner should work

--- bot.rb
+++ bot.rb
@@ -6,12 +6,20 @@ require "faraday"
 require "json"
 require "rufus-scheduler"
 require_relative "modules/apod"
+require_relative "modules/link_cleaner"
 
 TOKEN = ENV["AFAB_BOT_TOKEN"]
 CHANNEL = ENV["APOD_CHANNEL"]
 
 bot = Discordrb::Bot.new(token: TOKEN)
 scheduler = Rufus::Scheduler.new
+
+bot.message(contains: "http") do |event|
+  urls = event.message.content.scan(%r{https?://\S+})
+  urls.filter! { |url| LinkCleaner::has_tracker?(url) }
+  urls.tap { |url| LinkCleaner::clean(url) }
+  urls.each { event.message.reply(url) }
+end
 
 bot.message(with_text: "!apod") do |event|
   channel = event.channel
