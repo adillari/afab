13176518c28e642f43b5fab8a848bda4ce1fdb4e
Adil Lari <adillari@pm.me>
2025-04-21 08:43:59 -0500

    safer

--- bot.rb
+++ bot.rb
@@ -40,6 +40,7 @@ def send_apod(channel)
   retries ||= 0
 
   msg_text, embed = APOD.create_embed
+  channel.send_message(msg_text) unless msg_text.nil? || msg_text == ""
   channel.send_message(msg_text) if msg_text
   channel.send_embed("", embed)
 rescue StandardError => e
